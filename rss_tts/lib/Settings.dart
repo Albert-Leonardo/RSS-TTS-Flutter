7d8c89548f95d260d006b1ea14245e2602a9e931
Albert-Leonardo <1201301966@student.mmu.edu.my>
2022-10-05 15:53:27 +0800

    Dark Mode

--- rss_tts/lib/Settings.dart
+++ rss_tts/lib/Settings.dart
@@ -2,10 +2,17 @@ import 'package:flutter/material.dart';
 import 'package:flutter/src/widgets/container.dart';
 import 'package:flutter/src/widgets/framework.dart';
 import 'package:rss_tts/NavBar.dart';
+import 'package:flutter_settings_screens/flutter_settings_screens.dart';
 
-class Settings extends StatelessWidget {
-  const Settings({super.key});
-
+class SettingsPage extends StatelessWidget {
+  const SettingsPage({super.key});
+  static const keyDarkMode = 'key-dark-mode';
+  Widget buildDarkMode() => SwitchSettingsTile(
+        settingKey: keyDarkMode,
+        leading: Icon(Icons.dark_mode),
+        title: 'Dark mode',
+        onChange: (isDarkMode) {},
+      );
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -13,6 +20,18 @@ class Settings extends StatelessWidget {
       appBar: AppBar(
         title: Text('Settings'),
       ),
+      body: SafeArea(
+          child: ListView(
+        padding: EdgeInsets.all(24),
+        children: [
+          SettingsGroup(
+            title: 'GENERAL',
+            children: <Widget>[
+              buildDarkMode(),
+            ],
+          )
+        ],
+      )),
     );
   }
 }
--- rss_tts/lib/main.dart
+++ rss_tts/lib/main.dart
@@ -1,10 +1,12 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_settings_screens/flutter_settings_screens.dart';
 import 'package:rss_tts/NavBar.dart';
 import 'package:rss_tts/RSS_Feed.dart';
 import 'package:rss_tts/Settings.dart';
 import 'package:rss_tts/rss_mainmenu.dart';
 
-void main() {
+Future main() async {
+  await Settings.init(cacheProvider: SharePreferenceCache());
   runApp(const MyApp());
 }
 
@@ -13,17 +15,20 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      title: 'Flutter Demo',
-      theme: ThemeData(),
-      themeMode: ThemeMode.dark,
-      darkTheme: ThemeData(brightness: Brightness.dark),
-      initialRoute: '/home',
-      routes: {
-        '/home': (context) => const RSS_mainmenu(),
-        '/settings': (context) => const Settings(),
-      },
+    final isDarkMode = Settings.getValue<bool>(SettingsPage.keyDarkMode, true);
+    return ValueChangeObserver<bool>(
+      cacheKey: SettingsPage.keyDarkMode,
+      defaultValue: true,
+      builder: (_, isDarkMode, __) => MaterialApp(
+        debugShowCheckedModeBanner: false,
+        title: 'Flutter Demo',
+        theme: isDarkMode ? ThemeData.dark() : ThemeData.light(),
+        initialRoute: '/home',
+        routes: {
+          '/home': (context) => const RSS_mainmenu(),
+          '/settings': (context) => const SettingsPage(),
+        },
+      ),
     );
   }
 }
