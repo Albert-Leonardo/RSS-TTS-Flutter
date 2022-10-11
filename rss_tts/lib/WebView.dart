2d37aad57cbd0ebf1c05eea7b81b6629746c7293
Albert-Leonardo <1201301966@student.mmu.edu.my>
2022-10-11 15:48:22 +0800

    TTS function (not working with MalaysiaKini)

--- rss_tts/lib/WebView.dart
+++ rss_tts/lib/WebView.dart
@@ -6,6 +6,7 @@ import 'package:rss_tts/NavBar.dart';
 import 'package:http/http.dart' as http;
 import 'package:html/dom.dart' as dom;
 import 'package:rss_tts/rss_mainmenu.dart';
+import 'package:flutter_tts/flutter_tts.dart';
 
 class WebView extends StatefulWidget {
   const WebView(
@@ -27,7 +28,35 @@ stringConverter(String? s) {
 }
 
 class _WebViewState extends State<WebView> {
+  final FlutterTts flutterTts = FlutterTts();
+
+  speak(String text) async {
+    await flutterTts.awaitSpeakCompletion(true);
+    await flutterTts.setLanguage('en-US');
+    await flutterTts.setPitch(1);
+    await flutterTts.speak(text);
+  }
+
+  stopSpeak() async {
+    await flutterTts.stop();
+  }
+
+  player() async {
+    while (ttsIndex < TTS.length) {
+      if (checkPlay == true) {
+        await speak(TTS[ttsIndex]);
+        ttsIndex++;
+      } else if (checkPlay == false) {
+        stopSpeak();
+        break;
+      }
+    }
+  }
+
   List<String> TTS = [];
+  int ttsIndex = 0;
+  bool checkPlay = true;
+  bool playerVisibility = true;
   queryString() {
     if (widget.rss.newsUrl.contains('aljazeera'))
       return 'div > p';
@@ -52,32 +81,38 @@ class _WebViewState extends State<WebView> {
     print(TTS);
   }
 
+  _popUp() {
+    print("Popup");
+  }
+
   double _progress = 0;
   late InAppWebViewController webView;
   GlobalKey<ScaffoldState> scaffoldkey = GlobalKey<ScaffoldState>();
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-        appBar: AppBar(
-          title: Text(stringConverter(widget.title)),
-        ),
-        body: Stack(
-          children: [
-            InAppWebView(
-              initialOptions: InAppWebViewGroupOptions(
-                  crossPlatform:
-                      InAppWebViewOptions(transparentBackground: true)),
-              initialUrlRequest:
-                  URLRequest(url: Uri.parse(stringConverter(widget.newsUrl))),
-              onWebViewCreated: (InAppWebViewController controller) {
-                print("Lollllllllllll");
-                getWebsiteData();
-                webView = controller;
-              },
-              onProgressChanged:
-                  (InAppWebViewController controller, int progress) {
-                /*if (progress >= 80) {
+    return WillPopScope(
+        onWillPop: _popUp(),
+        child: Scaffold(
+            appBar: AppBar(
+              title: Text(stringConverter(widget.title)),
+            ),
+            body: Stack(
+              children: [
+                InAppWebView(
+                  initialOptions: InAppWebViewGroupOptions(
+                      crossPlatform:
+                          InAppWebViewOptions(transparentBackground: true)),
+                  initialUrlRequest: URLRequest(
+                      url: Uri.parse(stringConverter(widget.newsUrl))),
+                  onWebViewCreated: (InAppWebViewController controller) {
+                    print("Lollllllllllll");
+                    getWebsiteData();
+                    webView = controller;
+                  },
+                  onProgressChanged:
+                      (InAppWebViewController controller, int progress) {
+                    /*if (progress >= 80) {
                   print("Lol22222222222222222222");
                   String? htmlString = '';
                   controller.getHtml().then((value) => htmlString = value);
@@ -92,19 +127,108 @@ class _WebViewState extends State<WebView> {
                   }
                 }
                 */
-                setState(() {
-                  _progress = progress / 100;
-                });
-              },
-              onLoadStop: (controller, url) {},
-            ),
-            _progress < 1
-                ? SizedBox(
-                    height: 3,
-                    child: LinearProgressIndicator(value: _progress),
-                  )
-                : SizedBox()
-          ],
-        ));
+                    if (progress > 70) {
+                      player();
+                    }
+                    setState(() {
+                      _progress = progress / 100;
+                    });
+                  },
+                  onLoadStop: (controller, url) {
+                    //player();
+                  },
+                ),
+                _progress < 1
+                    ? SizedBox(
+                        height: 3,
+                        child: LinearProgressIndicator(value: _progress),
+                      )
+                    : SizedBox(),
+                if (playerVisibility)
+                  Align(
+                    alignment: Alignment.bottomCenter,
+                    child: Container(
+                      width: double.infinity,
+                      height: 100,
+                      decoration: BoxDecoration(
+                        color: Theme.of(context).bottomAppBarColor,
+                        borderRadius: BorderRadius.only(
+                          topLeft: Radius.circular(30.0),
+                          topRight: Radius.circular(30.0),
+                        ),
+                      ),
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        crossAxisAlignment: CrossAxisAlignment.center,
+                        children: [
+                          IconButton(
+                            iconSize: 62.0,
+                            color: Colors.blue,
+                            onPressed: () {},
+                            icon: Icon(
+                              Icons.skip_previous,
+                            ),
+                          ),
+                          IconButton(
+                              icon: Icon(
+                                  checkPlay ? Icons.pause : Icons.play_arrow),
+                              iconSize: 62.0,
+                              color: Colors.blue[800],
+                              onPressed: () {
+                                setState(() {
+                                  checkPlay = !checkPlay;
+                                  player();
+                                });
+                              }),
+                          IconButton(
+                              icon: Icon(Icons.skip_next),
+                              iconSize: 62.0,
+                              color: Colors.blue,
+                              onPressed: () {}),
+                          IconButton(
+                              icon: Icon(Icons.keyboard_arrow_down),
+                              iconSize: 62.0,
+                              color: Colors.blue,
+                              onPressed: () {
+                                setState(() {
+                                  playerVisibility = !playerVisibility;
+                                });
+                              }),
+                        ],
+                      ),
+                    ),
+                  ),
+                if (!playerVisibility)
+                  Align(
+                      alignment: Alignment.bottomCenter,
+                      child: Container(
+                          width: 50,
+                          height: 50,
+                          decoration: BoxDecoration(
+                            color: Theme.of(context).bottomAppBarColor,
+                            borderRadius: BorderRadius.only(
+                              topLeft: Radius.circular(30.0),
+                              topRight: Radius.circular(30.0),
+                            ),
+                          ),
+                          child: Row(
+                              mainAxisAlignment: MainAxisAlignment.center,
+                              crossAxisAlignment: CrossAxisAlignment.center,
+                              children: [
+                                IconButton(
+                                  iconSize: 30.0,
+                                  color: Colors.blue,
+                                  onPressed: () {
+                                    setState(() {
+                                      playerVisibility = !playerVisibility;
+                                    });
+                                  },
+                                  icon: Icon(
+                                    Icons.keyboard_arrow_up,
+                                  ),
+                                ),
+                              ])))
+              ],
+            )));
   }
 }
